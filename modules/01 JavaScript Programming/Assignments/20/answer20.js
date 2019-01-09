8364c32a8201fb79863c1f6ecac0235a362c3fc2
Moshiko <moshiko.albhari@gmail.com>
2019-01-09 12:13:36 +0200

    Update answer20.js

--- modules/01 JavaScript Programming/Assignments/20/answer20.js
+++ modules/01 JavaScript Programming/Assignments/20/answer20.js
@@ -1,8 +1,14 @@
-var arr = [1, 2, 5, 7];
+var arr = [1, 2, 5, 7, 4];
 
-function myFilter(arr, fnc) {
+function myFilter(arr, func) {
+        var newArr = [];
 
-        return arr.filter(fnc)
+        arr.forEach(value => {
+                if (func(value))
+                        newArr.push(value);
+        });
+
+        return newArr;
 }
 
 var filtered = myFilter(arr, function (item) {
